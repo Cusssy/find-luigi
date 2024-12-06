807a12defb192c8a0bf9945d88c3375d367d5207
Cusssy <71029516+Cusssy@users.noreply.github.com>
2024-12-06 23:21:17 +0100

    minijuego de luigi creado

---
+++ char.js
@@ -0,0 +1,78 @@
+export var pelotas = [];
+export var maxWidth = window.innerWidth - 100;
+export var maxHeight = window.innerHeight - 100;
+export var numPelotas = 15; // Número de pelotas
+export var speed = 5; // Velocidad constante en píxeles por cuadro
+
+// Función para crear una nueva pelota
+export function mario() {
+  var pelota = document.createElement('div');
+  pelota.className = 'mario';
+  document.body.appendChild(pelota);
+
+  // Posicionar la pelota en el centro de la página
+  pelota.style.left = Math.random() * maxWidth + 'px';
+  pelota.style.top = Math.random() * maxHeight + 'px';
+
+  // Asignar la velocidad constante en todas las direcciones
+  var angle = Math.random() * Math.PI * 2;
+  pelota.dx = Math.cos(angle) * speed;
+  pelota.dy = Math.sin(angle) * speed;
+
+  // Agregar la pelota al arreglo
+  pelotas.push(pelota);
+}
+
+
+export function luigi() {
+  var pelota = document.createElement('div');
+  pelota.className = 'luigi';
+  document.body.appendChild(pelota);
+
+  // Posicionar la pelota en el centro de la página
+  pelota.style.left = Math.random() * maxWidth + 'px';
+  pelota.style.top = Math.random() * maxHeight + 'px';
+
+  // Asignar la velocidad constante en todas las direcciones
+  var angle = Math.random() * Math.PI * 2;
+  pelota.dx = Math.cos(angle) * speed;
+  pelota.dy = Math.sin(angle) * speed;
+
+  // Agregar la pelota al arreglo
+  pelotas.push(pelota);
+}
+
+export function yoshi() {
+  var pelota = document.createElement('div');
+  pelota.className = 'yoshi';
+  document.body.appendChild(pelota);
+
+  // Posicionar la pelota en el centro de la página
+  pelota.style.left = Math.random() * maxWidth + 'px';
+  pelota.style.top = Math.random() * maxHeight + 'px';
+
+  // Asignar la velocidad constante en todas las direcciones
+  var angle = Math.random() * Math.PI * 2;
+  pelota.dx = Math.cos(angle) * speed;
+  pelota.dy = Math.sin(angle) * speed;
+
+  // Agregar la pelota al arreglo
+  pelotas.push(pelota);
+}
+export function wario() {
+  var pelota = document.createElement('div');
+  pelota.className = 'wario';
+  document.body.appendChild(pelota);
+
+  // Posicionar la pelota en el centro de la página
+  pelota.style.left = Math.random() * maxWidth + 'px';
+  pelota.style.top = Math.random() * maxHeight + 'px';
+
+  // Asignar la velocidad constante en todas las direcciones
+  var angle = Math.random() * Math.PI * 2;
+  pelota.dx = Math.cos(angle) * speed;
+  pelota.dy = Math.sin(angle) * speed;
+
+  // Agregar la pelota al arreglo
+  pelotas.push(pelota);
+}
---
+++ script.js
@@ -0,0 +1,55 @@
+import { numPelotas, pelotas, maxWidth, maxHeight } from './char.js';
+import { mario, yoshi, wario, luigi } from './char.js';
+
+
+
+
+// Función para mover las pelotas
+function movePelotas() {
+  for (var i = 0; i < pelotas.length; i++) {
+    var pelota = pelotas[i];
+    var currentLeft = parseFloat(pelota.style.left) || (maxWidth / 2);
+    var currentTop = parseFloat(pelota.style.top) || (maxHeight / 2);
+
+    // Verificar colisiones con los bordes horizontales
+    if (currentLeft + pelota.dx >= maxWidth || currentLeft + pelota.dx <= 0) {
+      pelota.dx *= -1; // Invertir dirección horizontalmente
+    }
+
+    // Verificar colisiones con los bordes verticales
+    if (currentTop + pelota.dy >= maxHeight || currentTop + pelota.dy <= 0) {
+      pelota.dy *= -1; // Invertir dirección verticalmente
+    }
+
+    // Actualizar la posición de la pelota
+    pelota.style.left = currentLeft + pelota.dx + 'px';
+    pelota.style.top = currentTop + pelota.dy + 'px';
+  }
+
+  // Repetir el movimiento a una tasa de actualización de 60fps
+  requestAnimationFrame(movePelotas);
+}
+
+// Crear múltiples pelotas
+for (var i = 0; i < numPelotas; i++) {
+  mario();
+  yoshi();
+  wario();
+}
+luigi();
+
+// Iniciar el movimiento de las pelotas
+movePelotas();
+
+
+//detecta el click a luigi
+document.querySelector('.luigi').addEventListener('click', function() {
+    alert("Luigi has been found!");
+});
+
+
+window.addEventListener("DOMContentLoaded", event => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.2;
+    audio.play();
+  });
